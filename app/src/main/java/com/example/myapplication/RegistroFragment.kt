04142251c840cc0080716f3ed5d9012e1b8ed9bd
SamuelEsteban16 <steban1614@gmail.com>
2025-04-07 16:46:16 -0500

    Primer commit

---
+++ app/src/main/java/com/example/myapplication/RegistroFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class RegistroFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_registro, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnRegister = view.findViewById<Button>(R.id.btn_register)
+
+        btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_registroFragment_to_categoriasFragment)
+        }
+    }
+}
