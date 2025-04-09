5b0e0520470b099ba330e8a3583339bc9e8a859b
SamuelEsteban16 <steban1614@gmail.com>
2025-04-08 22:18:00 -0500

    Agrupacion de fragments

--- app/src/main/java/com/example/myapplication/ColeccionEnlacesFragment.kt
+++ app/src/main/java/com/example/myapplication/ColeccionEnlacesFragment.kt
@@ -8,12 +8,12 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 
-class CategoriasFragment : Fragment() {
+class ColeccionEnlacesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_categorias, container, false)
+        val view = inflater.inflate(R.layout.fragments_colection_enlaces, container, false)
 
         val drawerLayout = view.findViewById<DrawerLayout>(R.id.drawer_layout)
         val menuIcon = view.findViewById<ImageView>(R.id.menu_icon)
